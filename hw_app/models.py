de3b083c2d821b6d9d7203efd6848408331a58ce
AngaPanga <w13w13@yandex.ru>
2023-09-02 05:00:43 +0300

    дз 4

--- hw_app/models.py
+++ hw_app/models.py
@@ -7,17 +7,10 @@ class User(models.Model):
     email = models.EmailField()
     mobile = models.CharField(max_length=20)
     us_adrs = models.CharField(max_length=100)
-    reg_day = models.DateTimeField(auto_now=True)
+    reg_day = models.DateField()
 
     def __str__(self):
         return f'{self.name}  -  {self.mobile} '
-
-    def full_data(self):
-        return f'{self.name}  -  ' \
-               f'{self.mobile}<br>' \
-               f'{self.email} <br>' \
-               f'{self.us_adrs} <br>' \
-               f'{self.reg_day}'
 
 
 # -------------- PRODUCT -----------------------------------------------------------------------
@@ -26,15 +19,11 @@ class Product(models.Model):
     content = models.TextField()
     price = models.DecimalField(default=1000.0, decimal_places=2, max_digits=10)
     count = models.IntegerField(default=1)
-    add_day = models.DateTimeField(auto_now=True)
+    add_day = models.DateField()
+    image = models.ImageField(upload_to='image/', default=None)
 
     def __str__(self):
-        return f'{self.name}  -  {self.price} '
-
-    def full_data(self):
-        return f'{self.name}  -  {self.price} <br>' \
-               f'{self.content}<br>' \
-               f'Count: {self.count}   Added at: {self.add_day}<br>'
+        return f'{self.name} - {self.price} '
 
     def price_get(self):
         return self.price
@@ -45,10 +34,10 @@ class Order(models.Model):
     us_name = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, related_name="order_products")
     sum_price = models.DecimalField(default=0.0, max_digits=10, decimal_places=2)
-    order_day = models.DateTimeField(auto_now=True)
+    order_day = models.DateField(auto_now=True)
 
     def __str__(self):
         return f'{self.us_name}' \
                f'Sum price: {self.sum_price}' \
-               f'Date: {self.order_day}' \
-               # f'{self.products}'
+               f'Date: {self.order_day}'
+
--- hw_app/views.py
+++ hw_app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import *
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import *
 from random import *
-from datetime import datetime
+from datetime import datetime, date
 
 
 # ------- Главная страница --------------------------------
@@ -14,15 +14,26 @@ def general(request):
     return render(request, "hw_app/general.html", context)
 
 
+# ------- Завершающая страница ----------------------------
+def complete(request):
+    context = {
+        "title": 'Изменения',
+        "head": 'Изменения',
+        "text": 'Изменения вступили в силу',
+    }
+    return render(request, "hw_app/complete.html", context)
+
+
 # ------- Тестовые данные ---------------------------------
 def fake_datas(request, urs, pds, ods):
-    for i in range(5):
-        if urs:
+    if urs:
+        for i in range(5):
             user = User(
                 name=f'Пользователь {i}',
                 email=f'e{i}@mail.com',
                 mobile=f'+7 (999) 555-44-0{i}',
                 us_adrs=f'Регион, Город, Улица, Дом, Кв.',
+                reg_day=date.today()
             )
             user.save()
     if pds:
@@ -32,14 +43,16 @@ def fake_datas(request, urs, pds, ods):
                 content=f'Описание товара ...',
                 price=uniform(500, 10000),
                 count=randint(1, 20),
+                add_day=date.today(),
+                image=None
             )
             product.save()
     if ods:
         for user in User.objects.all():
-            order = Order(us_name=user)
+            order = Order(us_name=user, order_day=date.today())
             order.save()
             sum_price = 0
-            l_pid = sample(list(i for i in range(1, len(Product.objects.all()))), 5)
+            l_pid = sample(list(i for i in range(1, len(Product.objects.all())+1)), 5)
             for pid in l_pid:
                 order_product = Product.objects.filter(pk=pid).first()
                 sum_price += order_product.price_get()
@@ -56,25 +69,42 @@ def fake_datas(request, urs, pds, ods):
 
 # ------- Список клентов ----------------------------------
 def list_users(request):
-    context = {"users": User.objects.all()}
+    context = {
+        "title": 'Список клиентов',
+        "head": 'Список клиентов',
+        "text": 'Список заргружен корректно',
+        "users": User.objects.all()
+    }
     return render(request, "hw_app/dbu.html", context)
 
 
 # ------- Список товаров ----------------------------------
 def list_products(request):
-    context = {"products": Product.objects.all()}
+    context = {
+        "title": 'Список товаров',
+        "head": 'Список товаров',
+        "text": 'Список заргружен корректно',
+        "products": Product.objects.all(),
+    }
     return render(request, "hw_app/dbp.html", context)
 
 
 # ------- Список заказов ----------------------------------
 def list_orders(request):
-    context = {"orders": Order.objects.all()}
+    context = {
+        "title": 'Список заказов',
+        "head": 'Список заказов',
+        "text": 'Список заргружен корректно',
+        "orders": Order.objects.all(),
+    }
     return render(request, "hw_app/dbo.html", context)
 
 
 # ------- Список товаров в заказе -------------------------
 def basket(request, oid):
     context = {
+        "title": 'Список товаров заказа',
+        "text": 'Список заргружен корректно',
         'head': Order.objects.get(pk=oid).us_name,
         'basket': Order.objects.get(pk=oid).products.all(),
     }
@@ -88,7 +118,12 @@ def us_products(request, uid):
     products_list = []
     for el in orders_list:
         products_list += Order.objects.get(pk=el.id).products.all()
-    context = {'user_products': products_list}
+    context = {
+        "title": 'Список клиента',
+        "head": 'Список товаров клиента',
+        "text": 'Список заргружен корректно',
+        'user_products': products_list,
+    }
     return render(request, 'hw_app/us_prod.html', context)
 
 
@@ -103,5 +138,152 @@ def us_products_time(request, uid, dif_day):
             products_list += Order.objects.get(pk=el.id).products.all()
     products_list = list(set(products_list))
     products_list.sort(key=lambda x: x.add_day)
-    context = {'user_products': products_list}
+    context = {
+        "title": 'Фил-ый список',
+        "head": 'Отфильтрованный список продуктов',
+        "text": 'Список заргружен корректно',
+        'user_products': products_list,
+    }
     return render(request, 'hw_app/us_prod.html', context)
+
+
+# ------------------------ ФОРМЫ --------------------------
+# --------------------- Список форм -----------------------
+def adds(request):
+    context = {
+        "title": 'Список форм',
+        "head": 'Список форм',
+    }
+    return render(request, 'hw_app/list_data.html', context)
+
+
+# ----------------- Создание клиента --------------
+def add_user(request):
+    if request.method == 'POST':
+        us_form(request)
+        return redirect('complete')
+    else:
+        form = UserForm()
+        context = {
+            "title": 'Форма клиента',
+            "head": 'Форма для заполнения клиента',
+            'form': form,
+            'button': 'Создать клиента'
+        }
+        return render(request, 'hw_app/form.html', context)
+
+
+# ----------------- Изменение клиента --------------
+def ch_user(request, uid):
+    if request.method == 'POST':
+        us_form(request, uid)
+        return redirect('complete')
+    else:
+        form = UserForm()
+        context = {
+            "title": 'Форма клиента',
+            "head": 'Форма для заполнения клиента',
+            'form': form,
+            'button': 'Изменить клиента'
+        }
+        return render(request, 'hw_app/form.html', context)
+
+
+# ----------------- Создание товара --------------
+def add_product(request):
+    if request.method == 'POST' and request.FILES:
+        prdt_form(request)
+        return redirect('complete')
+    else:
+        form = ProductForm()
+        context = {
+            "title": 'Форма товара',
+            "head": 'Форма для заполнения товара',
+            "button": 'Создать товар',
+            'form': form,
+        }
+        return render(request, 'hw_app/form.html', context)
+
+
+# ----------------- Изменение товара --------------
+def ch_product(request, pid):
+    if request.method == 'POST' and request.FILES:
+        prdt_form(request, pid)
+        return redirect('complete')
+    else:
+        form = ProductForm()
+        context = {
+            "title": 'Форма товара',
+            "head": 'Форма для заполнения товара',
+            'form': form,
+            'button': 'Изменить товар'
+        }
+        return render(request, 'hw_app/form.html', context)
+
+
+# ----------------- Универсальная форма клиента --------------
+def us_form(request, uid=None):
+    form = UserForm(request.POST)
+    if form.is_valid():
+        if uid:
+            user = User.objects.filter(pk=uid).first()
+        else:
+            user = User()
+            # user.save()
+        user.name = form.cleaned_data['name']
+        user.email = form.cleaned_data['email']
+        user.mobile = form.cleaned_data['mobile']
+        user.us_adrs = form.cleaned_data['us_adrs']
+        user.reg_day = form.cleaned_data['reg_day']
+        user.save()
+
+
+# ----------------- Универсальная форма товара --------------
+def prdt_form(request, pid=None):
+    form = ProductForm(request.POST, request.FILES)
+    if form.is_valid():
+        if pid:
+            product = Product.objects.filter(pk=pid).first()
+        else:
+            product = Product()
+        product.name = form.cleaned_data['name']
+        product.content = form.cleaned_data['content']
+        product.count = form.cleaned_data['count']
+        product.price = form.cleaned_data['price']
+        product.add_day = form.cleaned_data['add_day']
+        product.image = form.cleaned_data['image']
+        product.save()
+
+
+# ----------------- Выбор клиента --------------
+def choice_u(request):
+    if request.method == 'POST':
+        form = ChoiceUser(request.POST)
+        if form.is_valid():
+            return redirect(f'/ch_u/{form.cleaned_data["change"]}/')
+    else:
+        form = ChoiceUser()
+        context = {
+            "title": 'Выбор клиента',
+            "head": 'Форма для выбора клиента',
+            'form': form,
+            'button': 'Выбрать'
+        }
+        return render(request, 'hw_app/form.html', context)
+
+
+# ----------------- Выбор товара --------------
+def choice_p(request):
+    if request.method == 'POST':
+        form = ChoiceProduct(request.POST)
+        if form.is_valid():
+            return redirect(f'/ch_p/{form.cleaned_data["change"]}/')
+    else:
+        form = ChoiceProduct()
+        context = {
+            "title": 'Выбор товара',
+            "head": 'Форма для выбора товара',
+            'form': form,
+            'button': 'Выбрать'
+        }
+        return render(request, 'hw_app/form.html', context)
